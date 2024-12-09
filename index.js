4ede2e720294007c930d8ab28b8791bad8a002cb
+ <abstract@DESKTOP-O34JH50.>
2024-12-09 22:54:18 +0300

    adds index.js

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
-
 // Write your code here
+//test one
+let num1= 2;
+let num2=31;
+let multiply= num1*num2;
+//test two
+let random= 1;
+random()
+//test three
+//create a variable 'mod'that calculates the remainder of dividing 'num3 by num4' and the remainder is 4.
+const num3= 20;
+const num4= 8;
+const mod= num3 % num4;//this will give the remainder 4
+//test four
+//create a variable 'max'that findsthe highest number in a set, and the value returned is 20.
+let max = Math.max(5, 10, 15, 20);//the maximum value in this set is 20
+
